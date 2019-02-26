81a4794c02c1130d0100078f27369400f71dc052
aaronBurns59 <aaronburns59@gmail.com>
2019-02-26 20:00:24 +0000

    Created a function that passes different data types as args

---
+++ printList.py
@@ -0,0 +1,10 @@
+def printList(l):
+    for x in l:
+        print(x)
+
+list1 = [0,1,2,3,4,5,6,7,8,9,10]
+list2 = ['apple', 'orange', 'banana', 'pear', 'kiwi']
+list3 = [0.1,2.0,3.3,4.5,8.5]
+
+printList(list3)
+
