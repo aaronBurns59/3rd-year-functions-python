d9866cac8acff187211c6ae861c0f45cd5aa6ca2
aaronBurns59 <aaronburns59@gmail.com>
2019-02-26 20:05:32 +0000

    added user input to choose which list to print out

--- printList.py
+++ printList.py
@@ -6,5 +6,14 @@ list1 = [0,1,2,3,4,5,6,7,8,9,10]
 list2 = ['apple', 'orange', 'banana', 'pear', 'kiwi']
 list3 = [0.1,2.0,3.3,4.5,8.5]
 
-printList(list3)
+choice = int(input("Enter which list you want 1, 2 or 3: "))
+
+if choice == 1:
+    list = list1
+elif choice == 2:
+    list = list2
+elif choice == 3:
+    list = list3 
+
+printList(list)
 
