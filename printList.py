0ba163dc9e036d30f5d93ba5155275f81e806bb4
aaronBurns59 <aaronburns59@gmail.com>
2019-02-26 20:09:09 +0000

    added while loop for input validation

--- printList.py
+++ printList.py
@@ -6,7 +6,11 @@ list1 = [0,1,2,3,4,5,6,7,8,9,10]
 list2 = ['apple', 'orange', 'banana', 'pear', 'kiwi']
 list3 = [0.1,2.0,3.3,4.5,8.5]
 
-choice = int(input("Enter which list you want 1, 2 or 3: "))
+choice = 0
+
+
+while choice <= 0 or choice > 3:
+    choice = int(input("Enter which list you want 1, 2 or 3: "))
 
 if choice == 1:
     list = list1
